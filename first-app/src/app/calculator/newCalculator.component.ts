5d261c7723f3baaf4ca59bb3b055f9ee57237074
Magesh <tkmagesh77@gmail.com>
2017-06-30 16:49:56 +0530

    Day-01 Complete

--- first-app/src/app/calculator/newCalculator.component.ts
+++ first-app/src/app/calculator/newCalculator.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CalculatorModel } from './CalculatorModel';
 
 @Component({
 	selector : 'calculator-two',
 	templateUrl : 'newCalculator.component.html'
 })
 export class NewCalculatorComponent{
-
+	model : CalculatorModel = new CalculatorModel();
 }
